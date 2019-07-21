d7f5aa14c5b5b328bd54c1316cdee8fc6547da3c
BATMOOSEMIKE <michaelthebanana@gmail.com>
2019-07-20 17:05:47 -0700

    Found most effective and most accurate solution for loading image input

--- DigitClassifier.py
+++ DigitClassifier.py
@@ -1,11 +1,15 @@
+#Imports all required modules
 from keras.models import load_model
 from PIL import Image, ImageFilter
 from keras.preprocessing import image
 from keras.preprocessing.image import img_to_array, load_img
 import numpy as np
+import matplotlib.pyplot
 
+#Load pre-trained model
 model = load_model('digit_classifier.h5')
 
+#Here's some code that could be used in the future to convert images to MNIST format
 '''
 img = Image.open('newsample5.png').convert('L')
 width = float(img.size[0])
@@ -24,7 +28,7 @@ else:
     nwidth = int(round((20.0 / height * width), 0))  # resize width according to ratio height
     if (nwidth == 0):  # rare case but minimum is 1 pixel
         nwidth = 1
-        # resize and sharpen
+        # resize and sharpenpython
     img = img.resize((nwidth, 20), Image.ANTIALIAS).filter(ImageFilter.SHARPEN)
     wleft = int(round(((28 - nwidth) / 2), 0))  # caculate vertical pozition
     convertedImage.paste(img, (wleft, 4))  # paste resized image on white canvas
@@ -33,16 +37,21 @@ array = np.array(convertedImage)
 array = array.astype('float32')
 array = array.reshape(1, 28, 28, 1)
 array /= 255
-
 '''
 
-img = load_img('sample5.png', color_mode = "grayscale", target_size=(28,28))
+
+#Loads and converts image to MNIST format
+img = load_img('sample9.png', color_mode = "grayscale", target_size=(28,28))
 img = img_to_array(img)
 img = img.reshape(1, 28, 28, 1)
 img = img.astype('float32')
 img = img/255.0
 
+#Displays converted image for debugging purposes
+matplotlib.pyplot.imshow(img.reshape(28,28), cmap = matplotlib.cm.Greys_r)
+matplotlib.pyplot.show()
 
-
+#Makes and prints prediction
 pred = model.predict(img)
 print(pred.argmax())
+
